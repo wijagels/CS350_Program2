292e89e6e184325c8c9419a217b8ccb505b78884
William Jagels <will.jagels@gmail.com>
2016-05-03 21:28:45 -0400

    Move constructors in FS, beginnings of import.

--- FileSystem.cpp
+++ FileSystem.cpp
@@ -1,3 +1,4 @@
+/* Copyright 2016 Sarude Dandstorm $ ORIGINAL MIX */
 #include "FileSystem.hpp"
 
 #include <fstream>
@@ -5,11 +6,49 @@
 #include <cassert>
 #include <string>
 
-bool FileSystem::import(std::string, std::string) { return true; }
+#include "./debug.h"
+
+/*
+ * Default constructor, uses default parameters as specified in program
+ * description
+ */
+FileSystem::FileSystem()
+    : FileSystem(32, 1024 * 1024, 1024, 10 * 1000, 128 * 1024, 40) {}
+
+FileSystem::FileSystem(uint segment_count, uint segment_size, uint block_size,
+                       uint max_files, uint max_file_size, uint imap_blocks)
+    : SEGMENT_COUNT(segment_count),
+      SEGMENT_SIZE(segment_size),
+      BLOCK_SIZE(block_size),
+      MAX_FILES(max_files),
+      MAX_FILE_SIZE(max_file_size),
+      IMAP_BLOCKS(imap_blocks),
+      imap_() {}
+
+bool FileSystem::import(std::string file_name, std::string destination) {
+  std::ifstream file;
+  file.open(file_name, std::ios::binary);
+  assert(file.is_open());
+  file.seekg(0, std::ios::end);
+  std::streampos size = file.tellg();
+  logd("File size %d", static_cast<int>(size));
+  if (size > MAX_FILE_SIZE) {
+    return false;
+  }
+  char *buf = new char[size];
+  file.seekg(0, std::ios::beg);
+  file.read(buf, size);
+  file.close();
+  return true;
+}
 
 bool FileSystem::remove(std::string) { return true; }
 
-std::string FileSystem::list() { return ""; }
+std::string FileSystem::list() {
+  std::stringstream ss;
+  ss << "== List of Files ==";
+  return ss.str();
+}
 
 bool FileSystem::exit() { return true; }
 
--- FileSystem.hpp
+++ FileSystem.hpp
@@ -2,6 +2,8 @@
 #pragma once
 #include <string>
 
+#include "Imap.hpp"
+
 using uint = unsigned int;
 
 void fs_read_block(char *, uint);
@@ -9,15 +11,9 @@ uint bytes_to_uint(char *);
 
 class FileSystem {
  public:
-  FileSystem() : FileSystem(32, 1024 * 1024, 1024, 10 * 1000, 128 * 1024, 40) {}
+  FileSystem();
   FileSystem(uint segment_count, uint segment_size, uint block_size,
-             uint max_files, uint max_file_size, uint imap_blocks)
-      : SEGMENT_COUNT(segment_count),
-        SEGMENT_SIZE(segment_size),
-        BLOCK_SIZE(block_size),
-        MAX_FILES(max_files),
-        MAX_FILE_SIZE(max_file_size),
-        IMAP_BLOCKS(imap_blocks) {}
+             uint max_files, uint max_file_size, uint imap_blocks);
   bool import(std::string, std::string);
   bool remove(std::string);
   std::string cat(std::string);
@@ -33,4 +29,5 @@ class FileSystem {
   const uint MAX_FILES;
   const uint MAX_FILE_SIZE;
   const uint IMAP_BLOCKS;
+  Imap imap_;
 };
